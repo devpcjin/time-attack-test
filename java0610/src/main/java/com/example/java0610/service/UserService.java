64ae45a2017006aa17762bd2350f133878a793e0
devpcjin <sgn03077@gmail.com>
2022-06-10 20:00:33 +0900

    Update java0610

--- java0610/src/main/java/com/example/java0610/service/UserService.java
+++ java0610/src/main/java/com/example/java0610/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.java0610.service;
 
+import com.example.java0610.domain.User;
 import com.example.java0610.domain.UserRepository;
+import com.example.java0610.domain.UserRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,11 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public L
+    public Long search(Long id, UserRequestDto requestDto){
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("아이디가 존재하지 않습니다.")
+        );
+        user.search(requestDto);
+        return user.getId();
+    }
 }
